7e026c8da5de6da0b32b5b396aceb3e16402c1ee
someawesomee <alexcreed28@gmail.com>
2025-02-15 14:13:38 +0300

    color

--- app/src/main/java/ru/messenger/app1/fragments/HomeFragment.kt
+++ app/src/main/java/ru/messenger/app1/fragments/HomeFragment.kt
@@ -27,11 +27,10 @@ class HomeFragment : Fragment() {
         val chatsList: RecyclerView = view.findViewById(R.id.chats)
         val chats = arrayListOf<Chat>()
 
-        chats.add(Chat(5, "2024-10-15 16:18:13", "Сервис", "Затяни болты...", "profile_5"))
-        chats.add(Chat(4, "2024-10-10 15:15:10", "Васёк", "Привет", "profile_4"))
-        chats.add(Chat(3, "2023-08-54 03:00:04", "Лебовски", "Какие деньги?", "profile_3"))
-        chats.add(Chat(2, "2020-06-17 14:05:40", "РазДва", "Картина у нас", "profile_2"))
-        chats.add(Chat(1, "2004-01-13 10:15:10", "Томми", "А что не так с трейлером?", "profile_1"))
+        chats.add(Chat(5, "2024-10-15 16:18:13", "MLteam", "DeepSeek плох", "ml_team"))
+        chats.add(Chat(4, "2024-10-10 15:15:10", "DevOps", "Привет", "devops"))
+        chats.add(Chat(3, "2023-08-54 03:00:04", "Business", "Какие деньги?", "business"))
+
 
         chatsList.layoutManager = LinearLayoutManager(requireContext())
         chatsList.adapter = ChatAdapter(chats, requireContext())
